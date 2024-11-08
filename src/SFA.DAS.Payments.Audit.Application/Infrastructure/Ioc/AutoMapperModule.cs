103eb5b4d37e8e152f845fba4f37a64d89af11b2
Dave Read <dave.read@citizenazuresfabisgov.onmicrosoft.com>
2024-11-08 14:13:16 +0000

    PV2-3198 update deprecated AutoMapper methods in profiles

--- src/SFA.DAS.Payments.Audit.Application/Infrastructure/Ioc/AutoMapperModule.cs
+++ src/SFA.DAS.Payments.Audit.Application/Infrastructure/Ioc/AutoMapperModule.cs
@@ -24,7 +24,7 @@ namespace SFA.DAS.Payments.Audit.Application.Infrastructure.Ioc
             builder.Register(c => new MapperConfiguration(
                     cfg =>
                 {
-                    cfg.AddProfiles(GetType().Assembly);
+                    cfg.AddMaps(GetType().Assembly);
                 }))
                 .AsSelf()
                 .SingleInstance();
