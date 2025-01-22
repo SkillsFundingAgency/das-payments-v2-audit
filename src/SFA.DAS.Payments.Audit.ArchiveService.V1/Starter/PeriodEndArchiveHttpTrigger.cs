8262c92b114b69edb8bea1dcf848cb45090096c4
Pramod Lawate <pramod.lawate@FujitsuMicrosoftPracticeUK.onmicrosoft.com>
2025-01-22 14:06:43 +0000

    Handle HTTP POST in PeriodEndArchiveHttpTrigger
    Updated PeriodEndArchiveHttpTrigger.cs to handle HTTP POST requests. Added new using directives for necessary namespaces. Introduced conditional checks for HTTP method and job ID, and updated logic to clear status and schedule orchestration instances accordingly. Enhanced logging and error handling to return detailed JSON responses.
    #PV2-3404

--- src/SFA.DAS.Payments.Audit.ArchiveService.V1/Starter/PeriodEndArchiveHttpTrigger.cs
+++ src/SFA.DAS.Payments.Audit.ArchiveService.V1/Starter/PeriodEndArchiveHttpTrigger.cs
@@ -5,7 +5,9 @@ using Microsoft.DurableTask.Client;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.Payments.Audit.ArchiveService.V1.Helper;
 using SFA.DAS.Payments.Audit.ArchiveService.V1.Orchestrators;
+using SFA.DAS.Payments.Model.Core.Audit;
 using SFA.DAS.Payments.Monitoring.Jobs.Messages.Commands;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SFA.DAS.Payments.Audit.ArchiveService.V1.Starter
 {
@@ -29,24 +31,44 @@ namespace SFA.DAS.Payments.Audit.ArchiveService.V1.Starter
                 RecordPeriodEndFcsHandOverCompleteJob periodEndFcsHandOverJob = await req.ReadFromJsonAsync<RecordPeriodEndFcsHandOverCompleteJob>();
                 if (periodEndFcsHandOverJob == null)
                 {
-                    return req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+                    var badRequestResponse = req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+                    await badRequestResponse.WriteAsJsonAsync("Error in ReadFromJsonAsync");
+                    return badRequestResponse;
                 }
 
-                await _entityHelper.ClearCurrentStatus(client, StatusHelper.EntityState.add);
+                if (req.Method == "post")
+                {
+                    await _entityHelper.ClearCurrentStatus(client, StatusHelper.EntityState.add);
 
-                string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(nameof(PeriodEndArchiveOrchestrator)
-                    , input: periodEndFcsHandOverJob
-                    , new StartOrchestrationOptions());
+                    string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(nameof(PeriodEndArchiveOrchestrator)
+                        , input: periodEndFcsHandOverJob
+                        , new StartOrchestrationOptions());
 
-                logger.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 
-                return await client.CreateCheckStatusResponseAsync(req, instanceId);
+                    return await client.CreateCheckStatusResponseAsync(req, instanceId);
+                }
+
+                var stateResponse = await _entityHelper.GetCurrentJobs(client) ?? new ArchiveRunInformation();
+                if (stateResponse.JobId != periodEndFcsHandOverJob.JobId.ToString())
+                {
+                    stateResponse.JobId = periodEndFcsHandOverJob.JobId.ToString();
+                    stateResponse.InstanceId = string.Empty;
+                    stateResponse.Status = "Queued";
+                }
+
+                var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+                await response.WriteAsJsonAsync(stateResponse);
+                return response;
+
             }
             catch (Exception ex)
             {
                 string error = $"Error while executing {nameof(PeriodEndArchiveHttpTrigger)} ";
                 logger.LogError(ex, error, ex.Message);
-                return req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+
+                var response = req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
+                await response.WriteAsJsonAsync(error);
+                return response;
             }
         }
     }
