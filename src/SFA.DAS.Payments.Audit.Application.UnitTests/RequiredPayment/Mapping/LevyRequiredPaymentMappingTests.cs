737dfab68ba4f048a738a087673e2658888d015c
abroodie <abroodie@outlook.com>
2026-04-07 11:42:09 +0100

    Added failing tests for mapping Course Type , Learning Type and Course Code

--- src/SFA.DAS.Payments.Audit.Application.UnitTests/RequiredPayment/Mapping/LevyRequiredPaymentMappingTests.cs
+++ src/SFA.DAS.Payments.Audit.Application.UnitTests/RequiredPayment/Mapping/LevyRequiredPaymentMappingTests.cs
@@ -1,6 +1,9 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.Model.Core.Audit;
 using SFA.DAS.Payments.Model.Core.Entities;
 using SFA.DAS.Payments.RequiredPayments.Messages.Events;
+using System;
 
 namespace SFA.DAS.Payments.Audit.Application.UnitTests.RequiredPayment.Mapping
 {
@@ -15,6 +18,15 @@ namespace SFA.DAS.Payments.Audit.Application.UnitTests.RequiredPayment.Mapping
                 ApprenticeshipId = 400L,
                 ApprenticeshipPriceEpisodeId = 800L
             };
+        }
+
+        [TestCase(CourseType.Apprenticeship, LearningType.Apprenticeship)]
+        [TestCase(CourseType.Apprenticeship, LearningType.FoundationApprenticeship)]
+        [TestCase(CourseType.ShortCourse, LearningType.ApprenticeshipUnit)]
+        public void Maps_CourseType(CourseType courseType, LearningType learningType)
+        {
+            PaymentEvent.CourseType = courseType;
+            Mapper.Map<RequiredPaymentEventModel>(PaymentEvent).CourseType.Should().Be(courseType);
         }
     }
 }
