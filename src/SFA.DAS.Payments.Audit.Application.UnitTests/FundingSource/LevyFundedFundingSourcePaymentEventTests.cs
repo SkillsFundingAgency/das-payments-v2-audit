7aac0de9647f51bd92b24e0518fa263126911e29
abroodie <abroodie@outlook.com>
2026-04-28 13:47:33 +0100

    Revert "PV2-3950 resolved merge conflicts"
    This reverts commit 61f3c0ce403dd6b90e0a056a851b7827c3a83031, reversing
    changes made to e00b28a466ba43f00d26baf9af9b41a165c8a671.

--- src/SFA.DAS.Payments.Audit.Application.UnitTests/FundingSource/LevyFundedFundingSourcePaymentEventTests.cs
+++ src/SFA.DAS.Payments.Audit.Application.UnitTests/FundingSource/LevyFundedFundingSourcePaymentEventTests.cs
@@ -1,9 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using SFA.DAS.Payments.FundingSource.Messages.Events;
-using SFA.DAS.Payments.Model.Core.Audit;
-using System;
-using FluentAssertions;
-using SFA.DAS.Payments.Model.Core.Entities;
 
 namespace SFA.DAS.Payments.Audit.Application.UnitTests.FundingSource
 {
@@ -16,14 +13,6 @@ namespace SFA.DAS.Payments.Audit.Application.UnitTests.FundingSource
             {
                 RequiredPaymentEventId = Guid.NewGuid()
             };
-        }
-
-        [TestCase(CourseType.Apprenticeship)]
-        [TestCase(CourseType.ShortCourse)]
-        public void Maps_CourseType(CourseType courseType)
-        {
-            PaymentEvent.CourseType = courseType;
-            Mapper.Map<FundingSourceEventModel>(PaymentEvent).CourseType.Should().Be(PaymentEvent.CourseType);
         }
     }
 }
--- src/SFA.DAS.Payments.Audit.Application.UnitTests/FundingSource/SfaFullyFundedFundingSourcePaymentEventTests.cs
+++ src/SFA.DAS.Payments.Audit.Application.UnitTests/FundingSource/SfaFullyFundedFundingSourcePaymentEventTests.cs
@@ -1,9 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using SFA.DAS.Payments.FundingSource.Messages.Events;
-using SFA.DAS.Payments.Model.Core.Audit;
-using SFA.DAS.Payments.Model.Core.Entities;
-using System;
-using FluentAssertions;
 
 namespace SFA.DAS.Payments.Audit.Application.UnitTests.FundingSource
 {
@@ -16,22 +13,6 @@ namespace SFA.DAS.Payments.Audit.Application.UnitTests.FundingSource
             {
                 RequiredPaymentEventId = Guid.NewGuid()
             };
-        }
-
-        [TestCase(CourseType.Apprenticeship)]
-        [TestCase(CourseType.FunctionalSkill)]
-        public void Maps_CourseType(CourseType courseType)
-        {
-            PaymentEvent.CourseType = courseType;
-            Mapper.Map<FundingSourceEventModel>(PaymentEvent).CourseType.Should().Be(PaymentEvent.CourseType);
-        }
-
-        [TestCase(LearningType.Apprenticeship)]
-        [TestCase(LearningType.FoundationApprenticeship)]
-        public void Maps_LearningType(LearningType learningType)
-        {
-            PaymentEvent.LearningAim.LearningType = learningType;
-            Mapper.Map<FundingSourceEventModel>(PaymentEvent).LearningType.Should().Be(learningType);
         }
     }
 }
